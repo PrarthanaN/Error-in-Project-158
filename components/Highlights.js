50226de1eba8a039d0b0affa1111950c9a76fd83
Prarthana Nirhali <72865717+PrarthanaN@users.noreply.github.com>
2022-07-13 21:31:18 +0530

    Add files via upload

---
+++ components/Highlights.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+      selectedItemId: { default: "", type: "string" },
+    },
+    
+    init: function () {
+      this.handleMouseEnterEvents();
+      this.handleMouseLeaveEvents();
+    },
+
+    handleMouseEnterEvents: function () {
+        // Mouse Enter Events
+        this.el.addEventListener("mouseenter", () => {
+          this.handlePlacesListState();
+        });
+    },
+
+    handleMouseLeaveEvents: function () {
+        // Mouse Leave Events
+        this.el.addEventListener("mouseleave", () => {
+          const {selectedItemId} = this.data
+          if (selectedItemId){
+            const el = document.querySelector(`#${selectedItemId}`);
+            const id = el.getAttribute("id")
+            if (id == selectedItemId){
+              el.setAttribute("material", {color: "#0077CC"})
+            }
+          }
+        })
+        
+    },
+});
